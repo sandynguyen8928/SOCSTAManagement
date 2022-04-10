7f76e1f2d3d1765ea508aa8f156e247c235f6850
Sarah Lee <sarahlee7544@gmail.com>
2022-04-09 22:35:58 -0400

    finished report

--- manage/writeOHR.php
+++ manage/writeOHR.php
@@ -1,9 +1,9 @@
 <?php
-  $comment = "\n".$_POST["Term"] . "," . $_POST["Course"] . "," . $_POST["Name"] . "," . $_POST["Email"] . "," . $_POST["Position"] . "," . $_POST["Job"] . ",\"" . $_POST["Hours"] . "\",\"" . $_POST["Location"] . "\"";
+  $comment = "\n".$_POST["Term"] . "," . $_POST["Course"] . "," . $_POST["Name"] . "," . $_POST["Email"] . "," . $_POST["Position"] . "," . $_POST["Job"] . ",\"" . $_POST["Hours"] . "\",\"" . $_POST["Location"] . "\",\"" . $_POST["Responsibilities"] . "\"";
    
   $file = fopen("OHR.csv", "a") or die("Unable to open file");
   fwrite($file, $comment);
   fclose($file); 
 
-  header("Location: OHR.php?Term=".$_POST["Term"] . "&Course=" . $_POST["Course"] . "&Name=". $_POST["Name"] . "&Email=" . $_POST["Email"] . "&Job=" . $_POST["Job"] . "&Hours=" . $_POST["Hours"] . "&Location=" . $_POST["Location"]);
+  header("Location: OHR.php?Submitted=Done");
 ?>
