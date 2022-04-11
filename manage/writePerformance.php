0e98b2de0b5ef89909684e5b4b4ac86fa739f1c1
Sarah Lee <sarahlee7544@gmail.com>
2022-04-10 23:33:25 -0400

    added prof_name to performance.csv

--- manage/writePerformance.php
+++ manage/writePerformance.php
@@ -1,9 +1,9 @@
 <?php
-  $comment = "\n".$_POST["Term"] . "," . $_POST["Course"] . "," . $_POST["TA"] . ",\"" . $_POST["Comment"] . "\"";
+  $comment = "\n".$_POST["Term"] . "," . $_POST["Course"] . "," . $_POST["Prof"] . "," . $_POST["TA"] . ",\"" . $_POST["Comment"] . "\"";
    
   $file = fopen("performance.csv", "a") or die("Unable to open file");
   fwrite($file, $comment);
   fclose($file); 
 
-  header("Location: performance.php?Term=".$_POST["Term"] . "&Course=" . $_POST["Course"] . "&TA=" . $_POST["TA"] . "&Comment=" . $_POST["Comment"]);
+  header("Location: performance.php?Term=".$_POST["Term"] . "&Course=" . $_POST["Course"] . "&Prof=" . $_POST["Prof"] . "&TA=" . $_POST["TA"] . "&Comment=" . $_POST["Comment"]);
 ?>
