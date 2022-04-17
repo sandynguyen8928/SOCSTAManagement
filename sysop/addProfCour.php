0fcedae16df088f694a63f02c8290f58bb6dc3f9
vannachang <47489824+vannachang@users.noreply.github.com>
2022-04-17 15:42:54 -0400

    Update addProfCour.php

--- sysop/addProfCour.php
+++ sysop/addProfCour.php
@@ -4,7 +4,8 @@
   if(isset($_POST["addButton"])){
       // Writes in file
       $file = fopen("database/profAndCour.csv", "a") or die("Unable to open file!");
-      $string = $_POST["term_month_year"].",".$_POST["course_num"].",".$_POST["course_name"].",".$_POST["instructor_assigned_name"]."\n";
+      $term_month_year = $_POST["term_month"]." ".$_POST["year"];
+      $string = $term_month_year.",".$_POST["course_num"].",".$_POST["course_name"].",".$_POST["instructor_assigned_name"]."\n";
       fwrite($file, $string);
       fclose($file);
 
@@ -22,8 +23,21 @@
         <!-- New User Info form -->
         <!-- Note: VSC throws an error "A 'return' statement can only be used within a function body" but can be disregarded -->
         <form name="addProfCourForm" id="addProfCourForm" action="addProfCour.php" method="POST" onsubmit= "return checkEmpty()">
-          <label for="term_month_year">Term Month Year</label> 
-          <input type="text" name="term_month_year" class="loginBox">
+          
+        <label for="term_month_year">Please select the term and year:</label> <br>
+          <select name="term_month" id="term_month" value="term_month">
+              <option type="text" value="Fall September">Fall September</option>
+              <option type="text" value="Winter January">Winter January</option>
+              <option type="text" value="Summer May">Summer May</option>
+          </select> 
+          <select name="year" id="year" value="year">
+              <option type="text" value="2022">2022</option>
+              <option type="text" value="2021">2021</option>
+              <option type="text" value="2020">2020</option>
+              <option type="text" value="2019">2019</option>
+              <option type="text" value="2018">2018</option>
+              <option type="text" value="2017">2017</option>
+          </select> <br>
   
           <label for="course_num">Course Number</label> <br>
           <input type="text" name="course_num" class="loginBox"><br>
@@ -45,7 +59,8 @@
     <!-- check for blank entries -->
     <script>
         function checkEmpty() {
-            var term_month_year = document.forms["addProfCourForm"]["term_month_year"].value;
+            var term_month = document.forms["addProfCourForm"]["term_month"].value;
+            var year = document.forms["addProfCourForm"]["year"].value;
             var course_num = document.forms["addProfCourForm"]["course_num"].value;
             var course_name = document.forms["addProfCourForm"]["course_name"].value;
             var instructor_assigned_name = document.forms["addProfCourForm"]["instructor_assigned_name"].value;
