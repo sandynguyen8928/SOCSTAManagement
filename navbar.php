1a8e293ffdb67450a74eac83886524dc2308959e
vannachang <47489824+vannachang@users.noreply.github.com>
2022-04-17 23:17:31 -0400

    conditional nav bar
    the conditional isn't active rn :// i havent figured out how to read the csv files for data so it rn it's going through the 'else' option that displays the nav bar as usual

--- navbar.php
+++ navbar.php
@@ -6,28 +6,87 @@
                 </div>";
     }
     else{
-    //else if ($_SESSION["user_type"] == "student")  {
         // display navbar
-        echo "<div class='nav'>
-                    <div class='navItem' id='rate'>
-                        <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
-                    </div>
-                    <div class='navItem' id='manage'>
-                        <p><a href='/manage/TAManage.php' class='navtitle'>Manage TA</a></p>
-                    </div>
-
-                    <div class='navItem' id='admin'>
-                        <p><a href='/admin/TAadmin.php' class='navtitle'>Administration</a></p>
-                    </div>
-
-                    <div class='navItem' id='sysop'>
-                        <p><a href='/sysop/index.php' class='navtitle'>System Operator</a></p>
-                    </div>
-
-                    <div class='navItem' id='logout'>
-                        <a href='/logout.php' class='navtitle'>Logout</a>
-                    </div>
+        if ($_SESSION["sysop"] == TRUE){
+            echo "<div class='nav'>
+                  <div class='navItem' id='rate'>
+                      <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
+                  </div>
+                  <div class='navItem' id='manage'>
+                      <p><a href='/manage/TAManage.php' class='navtitle'>Manage TA</a></p>
+                  </div>
+                  <div class='navItem' id='admin'>
+                      <p><a href='/admin/TAadmin.php' class='navtitle'>Administration</a></p>
+                  </div>
+                  <div class='navItem' id='sysop'>
+                      <p><a href='/sysop/index.php' class='navtitle'>System Operator</a></p>
+                  </div>
+                  <div class='navItem' id='logout'>
+                      <a href='/logout.php' class='navtitle'>Logout</a>
+                  </div>
                 </div>";
+        }
+        else if ($_SESSION["administrator"] == TRUE)
+        {
+          echo "<div class='nav'>
+                <div class='navItem' id='rate'>
+                    <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
+                </div>
+                <div class='navItem' id='manage'>
+                    <p><a href='/manage/TAManage.php' class='navtitle'>Manage TA</a></p>
+                </div>
+                <div class='navItem' id='admin'>
+                      <p><a href='/admin/TAadmin.php' class='navtitle'>Administration</a></p>
+                  </div>
+                <div class='navItem' id='logout'>
+                    <a href='/logout.php' class='navtitle'>Logout</a>
+                </div>
+                </div>";
+        }
+        else if ($_SESSION["ta"] == TRUE || $_SESSION["professor"] == TRUE){
+          echo "<div class='nav'>
+                <div class='navItem' id='rate'>
+                    <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
+                </div>
+                <div class='navItem' id='manage'>
+                    <p><a href='/manage/TAManage.php' class='navtitle'>Manage TA</a></p>
+                </div>
+                <div class='navItem' id='logout'>
+                    <a href='/logout.php' class='navtitle'>Logout</a>
+                </div>
+                </div>";
+        }
+        else if ($_SESSION["student"] == TRUE){
+            echo "<div class='nav'>
+                <div class='navItem' id='rate'>
+                    <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
+                </div>
+                <div class='navItem' id='logout'>
+                    <a href='/logout.php' class='navtitle'>Logout</a>
+                </div>
+                </div>";
+        }
+        else {
+          echo "<div class='nav'>
+                <div class='navItem' id='rate'>
+                    <p><a href='/rate/index.php' class='navtitle'>Rate a TA</a></p>
+                </div>
+                <div class='navItem' id='manage'>
+                    <p><a href='/manage/TAManage.php' class='navtitle'>Manage TA</a></p>
+                </div>
+                <div class='navItem' id='admin'>
+                    <p><a href='/admin/TAadmin.php' class='navtitle'>Administration</a></p>
+                </div>
+
+                <div class='navItem' id='sysop'>
+                    <p><a href='/sysop/index.php' class='navtitle'>System Operator</a></p>
+                </div>
+
+                <div class='navItem' id='logout'>
+                    <a href='/logout.php' class='navtitle'>Logout</a>
+                </div>
+            </div>";
+        }
     }
 ?>
 
